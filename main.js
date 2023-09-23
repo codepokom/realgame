6bd64b628ccd46a97ef79b41a08ea8615eebdb9e
pokom <korotic@naver.com>
2023-09-23 15:16:31 +0900

    wow

--- main.js
+++ main.js
@@ -15,10 +15,17 @@ const right = "right";
 const left = "left";
 const NORMAL = "normal"
 const CHARGE = "charge"
+const BOSS = "boss"
 ////////////이미지 불러오기
 
 const img_villain = new Image();
 img_villain.src = 'src/villain1.png'
+//
+const img_c_villainRight = new Image();
+img_c_villainRight.src = 'src/c_villainRight.png'
+
+const img_c_villainLeft = new Image();
+img_c_villainLeft.src = 'src/c_villainLeft.png'
 //
 const img_actorRight = new Image();
 img_actorRight.src = 'src/actorRight.png'
@@ -85,7 +92,9 @@ let P_Up = false;
 let falling = false;
 let P_Space = false;
 let 공격중 = false;
+let onHit = false;
 let timer = 0;
+let ultimer = 0;
 let stack = 0;
 let ulti = false;
 let ultimatePoint = 0;
@@ -93,7 +102,7 @@ let imgFrameR = 0;
 let imgFrameL = 0;
 let killNum = 0;
 let MOVING = false;
-
+let tempLifePoint = 100;
 let HEADING_POINT = right;
 
 const villainOnScreen = [];
@@ -154,10 +163,10 @@ class ChargingVillain {
     this.MOVINGPOINT = MOVINGPOINT;
     this.turn = false;
     this.stun = false;
+    this.stunPoint = undefined;
   }
   draw(img) {
-    ctx.fillStyle = "red"
-    ctx.fillRect(this.x, this.y, 70, 70);
+    ctx.drawImage(img, this.x, this.y)
   }
   kill() {
     this.dead = true;
@@ -171,6 +180,53 @@ class ChargingVillain {
 }
 
 const c_villain1 = new ChargingVillain(0.85*CAN_WID, 0.7*CAN_HEI+5, left);
+const c_villain2 = new ChargingVillain(0.15*CAN_WID, 0.7*CAN_HEI+5, right);
+const c_villain3 = new ChargingVillain(0.85*CAN_WID, 0.7*CAN_HEI+5, left);
+const c_villain4 = new ChargingVillain(0.15*CAN_WID, 0.7*CAN_HEI+5, right);
+
+class BossOne{
+  constructor(x, y) {
+    this.type = "boss";
+    this.x = x;
+    this.y = y;
+    this.width = 150;
+    this.height = 200;
+    this.dead = false;
+    this.lifePoint = 100;
+  }
+  draw(img) {
+    ctx.fillStyle = "black"
+    ctx.fillRect(this.x, GROUND-200, 150, 200);
+  }
+  kill() {
+    this.dead = true;
+    killNum = parseInt(killNum) + 1;
+    if (ultimatePoint < 100) {
+      ultimatePoint += 30;
+    } else if (ultimatePoint >= 100) {
+      ultimatePoint = 100;
+    }
+  }
+}
+const b_stage_1 = new BossOne(0.5*CAN_WID, 0.7*CAN_HEI+5);
+
+class ultimateSkill{
+  constructor(x, y) {
+    this.type = ulti;
+    this.x = x;
+    this.y = y;
+    this.width = 400;
+    this.height = 175;
+  }
+  draw(img) {
+    ctx.fillStyle = "purple"
+    ctx.fillRect(this.x, this.y, 400, 175);
+  }
+}
+
+const ultimateSkill1 = new ultimateSkill(0,0)
+
+
 //주인공 공격 히트는 frame 안에다 ..
 // 실험 콘솔 주함수 밖
 console.log(`Ultimate Point: ${ultimatePoint}`);
@@ -185,13 +241,17 @@ function framework1() {
   //ctx.fillText(`Ultimate Point: ${ultimatePoint}`, 0.1*CAN_WID, 0.9*CAN_HEI);
   ctx.fillText(`Ultimate Point: ${ultimatePoint}`, 10, 30);
   actor.draw();
+  if(ultimatePoint > 100) {
+    ultimatePoint = 100;
+  }
   villainOnScreen.forEach((a, i, o)=> {
     if (a.dead === true) {
       a.x = -999;
       o.splice(i, 1);
     }
-    a.draw(img_villain);
+    
     if (a.type === NORMAL) {
+      a.draw(img_villain);
       if (a.MOVINGPOINT === left) {
         a.x -= 1.5;    
       } else if (a.MOVINGPOINT === right) {
@@ -199,15 +259,27 @@ function framework1() {
       }
     } else if (a.type === CHARGE) {
       if (a.MOVINGPOINT === left) {
+        a.draw(img_c_villainLeft)
         a.x -= 5.3;
-        console.log(a.x);
+        //console.log(a.x);
       } else if (a.MOVINGPOINT === right) {
+        a.draw(img_c_villainRight)
         a.x += 5.3;
+      } else if (a.stunPoint === 'left') {
+        a.draw(img_c_villainLeft)
+      } else if (a.stunPoint === 'right') {
+        a.draw(img_c_villainRight)
+      }
+    } else if (a.type === BOSS) {
+      a.draw()
+      if (a.lifePoint <= 0) {
+        a.kill();
       }
     }
     
-    
   })
+
+
 
   /////------------빌런  더  생성
   if (villain_R.dead === true && killNum === 1) {
@@ -221,7 +293,16 @@ function framework1() {
     const villain_L2 = new Villain(NORMAL, 0.05*CAN_WID, 0.7*CAN_HEI+10, right);
     villainOnScreen.push(villain_R2, villain_L2);
     killNum += 0.1;
-  }
+  } else if (killNum === 5 && c_villain1.dead === true) {
+    villainOnScreen.push(c_villain2);
+    killNum += 0.1;
+  } else if (killNum === 6 && c_villain2.dead === true) {
+    villainOnScreen.push(c_villain3, c_villain4);
+    killNum += 0.1;
+  } else if (killNum === 8) {
+    villainOnScreen.push(b_stage_1)
+    killNum += 0.1;
+  } 
   //////-------------------좌우 이동
   if (P_Left) {
     actor.x -= 1.5;
@@ -306,19 +387,35 @@ function framework1() {
     villainOnScreen.forEach((a, i, o) => {
       if (HEADING_POINT === right) {
         if ((a.x - attack_A.width <= attack_A.x && attack_A.x <= a.x + attack_A.width) && (a.y - attack_A.height <= attack_A.y && attack_A.y <= a.y + a.height)) {
-          if (a.type !== CHARGE) {
+          if (a.type !== CHARGE && a.type !== BOSS ) {
             a.kill();
           } else if (a.type === CHARGE) {
             if(a.MOVINGPOINT !== "0") {
             console.log("Blocked")
             } else if (a.MOVINGPOINT === "0") {
               a.kill();
+            } 
+          } else if (a.type === BOSS) {
+            onHit = true
+            switch (a.lifePoint) {
+              case 100:
+                a.lifePoint = 76;
+                break;
+              case 75:
+                a.lifePoint = 51;
+                break;
+              case 50:
+                a.lifePoint = 26;
+                break;
+              case 25:
+                a.lifePoint = 0;
+                break;
             }
           }
         console.log("kill him")
       }} else if (HEADING_POINT === left) {
         if ((a.x <= attack_A_L.x && a.x + a.width >= attack_A_L.x) && (a.y <= attack_A_L.height + attack_A_L.y && attack_A_L.y <= a.y + a.height)) {
-          if (a.type !== CHARGE) {
+          if (a.type !== CHARGE  && a.type !== BOSS) {
             a.kill();
           } else if (a.type === CHARGE) {
             if(a.MOVINGPOINT !== "0") {
@@ -326,15 +423,69 @@ function framework1() {
               } else if (a.MOVINGPOINT === "0") {
                 a.kill();
               }
+          } else if (a.type === BOSS) {
+            switch (a.lifePoint) {
+              case 100:
+                a.lifePoint = 76;
+                break;
+              case 75:
+                a.lifePoint = 51;
+                break;
+              case 50:
+                a.lifePoint = 26;
+                break;
+              case 25:
+                a.lifePoint = 0;
+                break;
+            }
           }
         }
       }
     })
   
   }
-    ///////공격끝
-    //=============궁극기
+  ///////공격끝
+  if (!공격중) {
+    switch(b_stage_1.lifePoint) {
+      case 76:
+        b_stage_1.lifePoint = 75
+        break;
+      case 51:
+        b_stage_1.lifePoint = 50
+        break;
+      case 26:
+        b_stage_1.lifePoint = 25
+        break;
+    }
+  } else if(!ulti) {
+    if(b_stage_1.lifePoint === 999) {
+      b_stage_1.lifePoint = 25;
+    }
+  }
+  //=============궁극기
+  if(ulti) {
+    ultimateSkill1.y = CAN_HEI*0.1 + 7*ultimer;
+      ultimer ++;
+      ultimateSkill1.draw()
     
+    //빌런 궁 피격 판정
+    villainOnScreen.forEach((a, i, o) => {
+      if((a.x <= ultimateSkill1.x + ultimateSkill1.width && ultimateSkill1.x <= a.x + a.width) && (a.y <= ultimateSkill1.y + ultimateSkill1.height && ultimateSkill1.y <= a.y + a.height))
+        if (a.type !== BOSS) {
+          a.kill()
+        } else {
+          if(a.lifePoint === 100) {
+            a.lifePoint = 999
+          } else if (a.lifePoint <= 75){
+            a.lifePoint = 0
+          }
+        }
+    })
+    if (ultimateSkill1.y + ultimateSkill1.height >= CAN_HEI*0.7 + 75) {
+      ulti = false
+      ultimer = 0;
+    }
+  }
     //-차저 방향전환 및 스턴
     villainOnScreen.forEach((a, i, o) => {
       if (a.type === CHARGE && a.MOVINGPOINT === left && a.x <= 0 ) {
@@ -342,6 +493,7 @@ function framework1() {
           a.stun = true;
           a.x = 0;
           a.MOVINGPOINT = "0";
+          a.stunPoint = 'left'
         } else if (a.turn === false) {
           a.turn = true;
           a.MOVINGPOINT = right;
@@ -351,6 +503,7 @@ function framework1() {
           a.stun = true;
           a.x = CAN_WID-70;
           a.MOVINGPOINT = "0";
+          a.stunPoint = 'right'
         } else if (a.turn === false) {
           a.turn = true;
           a.MOVINGPOINT = left;
@@ -366,7 +519,7 @@ function framework1() {
   }; */
   //이거로하면 위의것을 일일ㅇ이쓰지않아도 되긴하는데 그냥 리턴하는 방법(return foreach => return a)은 없는건가>?
   villainOnScreen.forEach((a, i, o) => {
-    if ((a.x - actor.width <= actor.x && actor.x <= a.x + actor.width) && (a.y - actor.height <= actor.y && actor.y <= a.y + actor.height)) {
+    if ((a.x <= actor.x + actor.width && actor.x <= a.x + a.width) && (a.y <= actor.y + actor.height && actor.y <= a.y + a.height)) {
       console.log("game over");
       home.classList.remove(class_HIDDEN)
       canvas.classList.add(class_HIDDEN);
@@ -374,7 +527,7 @@ function framework1() {
   })
   //
 
-  
+  //console.log(b_stage_1.lifePoint)
 }
 
 function playAgain(e) {
@@ -424,9 +577,14 @@ document.addEventListener("keydown", function(e) {
 })
 
 document.addEventListener("keydown", function(e) {
-  if (e.code === "KeyR") {
+  if (e.code === "KeyR" && ultimatePoint === 100) {
     ultimatePoint = 0;
     ulti = true;
+    if(HEADING_POINT === right) {
+      ultimateSkill1.x = actor.x + actor.width + 100
+    } else if(HEADING_POINT === left) {
+      ultimateSkill1.x = actor.x - 100 - ultimateSkill1.width
+    }
   }
 })
 
